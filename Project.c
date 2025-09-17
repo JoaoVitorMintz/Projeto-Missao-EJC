2ef33db5011dabe512df93a83e17c5869ce80c89
João Vitor G A Mintz <joaomintzz@gmail.com>
2025-09-16 21:09:50 -0300

    UPDATED: Procurar supostamente funciona, mas deve realizar testes, pode cair em loop infinito se tiver um H e outro H um em baixo do outro, portanto, devemos procurar uma forma de resolver isso

--- Project.c
+++ Project.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 void construirMatriz(int linhas, int colunas, char** v);
+int procurar(int posLinhas, int posColunas, char** v, int tamLinha, int tamColuna);
 
-//int procurar(int linhas, int coluna, int char** v, );
+// TODO: REALIZAR OS TESTES!
 
 int main () {
-    int n, m; // n são as linhas (andares) e m são as colunas (quantidade de quartos);
+    int n, m, posLinha, posColuna; // n são as linhas (andares) e m são as colunas (quantidade de quartos);
 
     printf("Indique o número de andares do prédio: ");
     scanf("%d", &n);
@@ -17,7 +18,7 @@ int main () {
     // Alocando memória para construção da matriz:
     char **v = malloc(n * sizeof(char *));
     if (v == NULL) {
-        perror("Erro ao alocar memória das linha");
+        perror("Erro ao alocar memória das linhas");
         return 1;
     } 
 
@@ -28,13 +29,28 @@ int main () {
             return 1;
         }
     }
+    
 
     construirMatriz(n, m, v);
 
+    printf("\nIndique o andar em que começará a porcurar: ");
+    scanf("%d", &posLinha);
+    printf("\nIndique a posição da sala que começará a procurar: ");
+    scanf("%d", &posColuna);
+
     // Apresentando a matriz inserida pelo usuário;
-    printf("\nMatriz final:\n");
+    printf("%d\n", n);
     for (int i = n - 1; i >= 0; i--) {
         printf("%s\n", v[i]);
+    }
+    printf("%d  %d\n", posLinha, posColuna);
+
+    int resposta = procurar(posLinha, posColuna, v, n, m);
+
+    if (resposta == 0) {
+        printf("Chave encontrada no Edifício João Calvino!");
+    } else {
+        printf("Não conseguimos encontrar a chave no Edifício João Calvino.");
     }
 
     for (int i = 0; i < n; i++) {
@@ -76,17 +92,21 @@ void construirMatriz(int linhas, int colunas, char** v) {
 }
 
 
-/*int procurar(int linhas, int colunas, int char** v){
-    if (v[linhas][colunas] == '*') {
-        printf("Chave encontrada! No andar %d e na sala %d", linhas, colunas);
-        return 0;
+int procurar(int posLinhas, int posColunas, char** v,  int tamLinha, int tamColuna) {
+    // Condições de parada (0 para caso de sucesso e 1 para falha ao buscar a chave)
+    if (v[posLinhas][posColunas] == '*') return 0;
+    if (posLinhas < 0 || posLinhas >= tamLinha || posColunas < 0 || posColunas >= tamColuna) return 1;
+
+
+    // Verificação da letra e realiza a chama recursiva com base na letra
+    if (v[posLinhas][posColunas] == 'H') {
+       if (procurar(posLinhas + 1, posColunas, v, tamLinha, tamColuna) == 0) return 0;
+       if (procurar(posLinhas - 1, posColunas, v, tamLinha, tamColuna) == 0) return 0;
+    } else if (v[posLinhas][posColunas] == 'V') {
+        if (procurar(posLinhas, posColunas + 1, v, tamLinha, tamColuna) == 0) return 0;
+        if (procurar(posLinhas, posColunas - 1, v, tamLinha, tamColuna) == 0) return 0;
     }
-    if (v[linhas][colunas] == 'H') {
-       procurar(linhas + 1, colunas, v);
-       procurar(linhas - 1, colunas, v);
-    } else if (v[linhas][colunas] == 'V') {
-        procurar(linhas, colunas + 1, v);
-        procurar(linhas, colunas - 1, v);
-    }
-    
-}*/
+
+    return 1;
+
+}
